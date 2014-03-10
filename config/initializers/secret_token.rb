e53b749f69e03f957d71cbf88c60fdff83aca6cd
alonzae <rushclass12>
2014-03-10 14:53:42 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirdApp::Application.config.secret_token = '6be2d305e15ea4cbcd462ec96958d671af1d38f38758b74c9a87f2729c0fe1678f77c0b39cbfdca1211d45a35abf61e1588b7ab0458931faf34dc73a6e5bbc05'
